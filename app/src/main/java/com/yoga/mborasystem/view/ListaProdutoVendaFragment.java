446e441ae052d3f6dde01ebec03732ccbf0b7ea0
António Teca <antonioteca@hotmail.com>
2021-10-30 01:13:11 +0100

    Toast de código Qr removido

--- app/src/main/java/com/yoga/mborasystem/view/ListaProdutoVendaFragment.java
+++ app/src/main/java/com/yoga/mborasystem/view/ListaProdutoVendaFragment.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.xwray.groupie.GroupAdapter;
 import com.xwray.groupie.GroupieViewHolder;
