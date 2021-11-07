d1db0fd178e992ba80d86d19f48ab4288337d496
António Teca <antonioteca@hotmail.com>
2021-11-07 18:00:32 +0100

    Refatoração SplashFragment

--- app/src/main/java/com/yoga/mborasystem/view/SplashFragment.java
+++ app/src/main/java/com/yoga/mborasystem/view/SplashFragment.java
@@ -9,37 +9,30 @@ import android.view.ViewGroup;
 
 import com.yoga.mborasystem.R;
 import com.yoga.mborasystem.util.Ultilitario;
-import com.yoga.mborasystem.viewmodel.ClienteViewModel;
 
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 public class SplashFragment extends Fragment {
 
-    private ClienteViewModel clienteViewModel;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        clienteViewModel = new ViewModelProvider(requireActivity()).get(ClienteViewModel.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            Ultilitario.getExisteMutableLiveData().observe(getViewLifecycleOwner(), existe -> {
-                switch (existe) {
-                    case SIM:
-                        Navigation.findNavController(getView()).navigate(R.id.action_splashFragment_to_loginFragment);
-                        break;
-                    case NAO:
-                        Navigation.findNavController(getView()).navigate(R.id.action_splashFragment_to_cadastrarClienteFragment);
-                        break;
-                }
-            });
-        }, 5000);
+        new Handler(Looper.getMainLooper()).postDelayed(() -> Ultilitario.getExisteMutableLiveData().observe(getViewLifecycleOwner(), existe -> {
+            switch (existe) {
+                case SIM:
+                    Navigation.findNavController(requireView()).navigate(R.id.action_splashFragment_to_loginFragment);
+                    break;
+                case NAO:
+                    Navigation.findNavController(requireView()).navigate(R.id.action_splashFragment_to_cadastrarClienteFragment);
+                    break;
+            }
+        }), 5000);
         return inflater.inflate(R.layout.fragment_splash, container, false);
     }
 }
