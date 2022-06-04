ebc3ff7e4f6133d74133cb9ae77f8379d577a24e
António Teca <antonioteca@hotmail.com>
2022-06-04 01:08:34 +0100

    abrir documento no formato PDF

--- app/src/main/java/com/yoga/mborasystem/view/DocumentoFragment.java
+++ app/src/main/java/com/yoga/mborasystem/view/DocumentoFragment.java
@@ -3,23 +3,37 @@ package com.yoga.mborasystem.view;
 import static com.yoga.mborasystem.util.Ultilitario.getPdfList;
 
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.xwray.groupie.GroupAdapter;
 import com.xwray.groupie.GroupieViewHolder;
 import com.xwray.groupie.Item;
+import com.yoga.mborasystem.MainActivity;
 import com.yoga.mborasystem.R;
 import com.yoga.mborasystem.databinding.FragmentDocumentoBinding;
 import com.yoga.mborasystem.util.Ultilitario;
 
+import java.io.File;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -68,16 +82,18 @@ public class DocumentoFragment extends Fragment {
             Ultilitario.naoEncontrado(getContext(), adapter, msg);
         } else {
             for (Ultilitario.Documento documento : pdfList)
-                adapter.add(new ItemDocumento(documento));
+                adapter.add(new ItemDocumento(documento, requireContext()));
         }
     }
 
     static class ItemDocumento extends Item<GroupieViewHolder> {
 
+        private Context context;
         private final Ultilitario.Documento documento;
 
-        public ItemDocumento(Ultilitario.Documento documento) {
+        public ItemDocumento(Ultilitario.Documento documento, Context context) {
             this.documento = documento;
+            this.context = context;
         }
 
         @SuppressLint("SetTextI18n")
@@ -87,6 +103,9 @@ public class DocumentoFragment extends Fragment {
             TextView descricao = viewHolder.itemView.findViewById(R.id.txtDescricao);
             nomeDocumento.setText(documento.getNome());
             descricao.setText(Ultilitario.converterData(documento.getData_modifica()) + " - " + formatSize(documento.getTamanho()));
+            viewHolder.itemView.setOnClickListener(v -> {
+                abrirDocumentoPDF(v);
+            });
         }
 
         @Override
@@ -94,7 +113,34 @@ public class DocumentoFragment extends Fragment {
             return R.layout.layout_documento_fragment;
         }
 
-        public String formatSize(long size) {
+        private void abrirDocumentoPDF(View v) {
+            v.setBackgroundColor(Color.parseColor("#6BD3D8D7"));
+            new Handler(Looper.getMainLooper()).postDelayed(() -> {
+                v.setBackgroundColor(Color.WHITE);
+            }, 1000);
+            File file = new File(documento.getCaminho());
+            Uri fileURI = FileProvider.getUriForFile(context, "com.yoga.mborasystem", file);
+            Intent target = new Intent(Intent.ACTION_VIEW);
+            target.setDataAndType(fileURI, "application/pdf");
+            target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+            target.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+            Intent intent = Intent.createChooser(target, context.getString(R.string.ab_fi));
+
+            PackageManager packageManager = context.getPackageManager();
+            List<ResolveInfo> activities = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+            try {
+                if (activities.size() > 0) {
+                    context.startActivity(intent);
+                } else {
+                    Ultilitario.alertDialog(context.getString(R.string.fal_ab_pdf), context.getString(R.string.inst_app), context, R.drawable.ic_baseline_store_24);
+                }
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        private String formatSize(long size) {
             if (size <= 0)
                 return "0";
             final String[] units = new String[]{"B", "KB", "MB", "GB", "TB"};
