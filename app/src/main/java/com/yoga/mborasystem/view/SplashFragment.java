eb196a1fdfc9483101878a7a4746405da50af302
António Teca <antonioteca@hotmail.com>
2022-06-27 22:10:21 +0100

    Configurações iniciais do atalho

--- app/src/main/java/com/yoga/mborasystem/view/SplashFragment.java
+++ app/src/main/java/com/yoga/mborasystem/view/SplashFragment.java
@@ -1,16 +1,20 @@
 package com.yoga.mborasystem.view;
 
+import static com.yoga.mborasystem.util.Ultilitario.Existe.NAO;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.yoga.mborasystem.R;
 import com.yoga.mborasystem.viewmodel.ClienteViewModel;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
@@ -19,6 +23,7 @@ import java.util.Objects;
 public class SplashFragment extends Fragment {
 
     private ClienteViewModel clienteViewModel;
+    private static final String composeFactura = "android.intent.action.VIEW";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,8 +39,12 @@ public class SplashFragment extends Fragment {
             clienteViewModel.getExisteMutableLiveData().observe(getViewLifecycleOwner(), existe -> {
                 switch (existe) {
                     case SIM:
-                        if (Objects.requireNonNull(Navigation.findNavController(requireView()).getCurrentDestination()).getId() == R.id.splashFragment)
-                            Navigation.findNavController(requireView()).navigate(R.id.action_splashFragment_to_loginFragment);
+                        if (composeFactura.equals(requireActivity().getIntent().getAction())) {
+//                            Navigation.findNavController(requireView()).navigate(R.id.action_splashFragment_to_atalho);
+                        } else {
+                            if (Objects.requireNonNull(Navigation.findNavController(requireView()).getCurrentDestination()).getId() == R.id.splashFragment)
+                                Navigation.findNavController(requireView()).navigate(R.id.action_splashFragment_to_loginFragment);
+                        }
                         break;
                     case NAO:
                         Navigation.findNavController(requireView()).navigate(R.id.action_splashFragment_to_cadastrarClienteFragment);
