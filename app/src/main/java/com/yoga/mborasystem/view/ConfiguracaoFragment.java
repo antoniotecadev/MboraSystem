b70a24fcc98e8b15f2932442c97dfb575ccc1e08
António Teca <antonioteca@hotmail.com>
2022-07-17 21:44:35 +0100

    desabilitar ou habilitar imposto do IVA e reaordenar as configurações

--- app/src/main/java/com/yoga/mborasystem/view/ConfiguracaoFragment.java
+++ app/src/main/java/com/yoga/mborasystem/view/ConfiguracaoFragment.java
@@ -3,28 +3,34 @@ package com.yoga.mborasystem.view;
 import static com.yoga.mborasystem.util.Ultilitario.getSelectedIdioma;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
-import com.yoga.mborasystem.R;
-
-import androidx.preference.EditTextPreference;
 import androidx.preference.ListPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
-import java.util.regex.Pattern;
+import com.yoga.mborasystem.R;
 
 public class ConfiguracaoFragment extends PreferenceFragmentCompat {
 
     private String codigoIdioma;
-    private final Pattern numero = Pattern.compile("[^0-9 ]");
+    private ListPreference motivoIsento;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences_config, rootKey);
         ListPreference listaIdioma = findPreference("lista_idioma");
-//        taxaIva.setOnPreferenceChangeListener((preference, newValue) -> {
-//            return true;
-//        });
+        ListPreference taxaIva = findPreference("taxa_iva");
+        motivoIsento = findPreference("motivo_isencao");
+
+        assert taxaIva != null;
+        desabilitarMotivoIsencao(taxaIva.getValue(), motivoIsento);
+
+        taxaIva.setOnPreferenceChangeListener((preference, newValue) -> {
+            int taxas = Integer.parseInt(newValue.toString());
+            String[] taxa_values = getResources().getStringArray(R.array.array_taxa_iva_valor);
+            String taxa = taxa_values[taxas == 14 ? 3 : taxas == 7 ? 2 : (taxas == 5 ? 1 : 0)];
+            desabilitarMotivoIsencao(taxa, motivoIsento);
+            return true;
+        });
         assert listaIdioma != null;
         listaIdioma.setOnPreferenceChangeListener((preference, newValue) -> {
             switch (newValue.toString()) {
@@ -43,5 +49,9 @@ public class ConfiguracaoFragment extends PreferenceFragmentCompat {
             getSelectedIdioma(requireActivity(), codigoIdioma, newValue.toString(), false, false);
             return true;
         });
+    }
+
+    private void desabilitarMotivoIsencao(String taxa, ListPreference motivoIsento) {
+        motivoIsento.setEnabled(taxa.equals("0"));
     }
 }
