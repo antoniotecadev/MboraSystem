d1ab67e596e572d115b75d75d45c007eb5f5a800
António Teca <antonioteca@hotmail.com>
2022-09-06 12:08:14 +0100

    pegar o nif gererico de cliente caso o nif estege vazio.

--- app/src/main/java/com/yoga/mborasystem/util/SaftXMLDocument.java
+++ app/src/main/java/com/yoga/mborasystem/util/SaftXMLDocument.java
@@ -130,7 +130,7 @@ public class SaftXMLDocument {
         if (clienteCantina.isEmpty())
             elementCustomer(doc, masterFiles, "1", "999999999", "Consumidor Final", "Desconhecido", "Desconhecido", "Desconhecido", "Desconhecido", "Desconhecido");
         else for (ClienteCantina cc : clienteCantina)
-            elementCustomer(doc, masterFiles, String.valueOf(cc.getId()), cc.getNif(), cc.getNome(), isEmpty(cc.getEndereco()), cliente.getProvincia(), cliente.getProvincia(), isEmpty(cc.getTelefone().trim()), isEmpty(cc.getEmail().trim()));
+            elementCustomer(doc, masterFiles, String.valueOf(cc.getId()), cc.getNif().isEmpty() ? "999999999" : cc.getNif(), cc.getNome(), isEmpty(cc.getEndereco()), cliente.getProvincia(), cliente.getProvincia(), isEmpty(cc.getTelefone().trim()), isEmpty(cc.getEmail().trim()));
 
         Map<Long, ProdutoVenda> produtoVendaMap = new HashMap<>();
         for (ProdutoVenda pv : produtoVendas)
