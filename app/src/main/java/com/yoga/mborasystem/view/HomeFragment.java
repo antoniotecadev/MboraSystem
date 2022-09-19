a1c7a51dfa0859fe57074c082291a37e11f3615f
António Teca <antonioteca@hotmail.com>
2022-09-19 20:29:01 +0100

    mostrar nome de usuário na home.

--- app/src/main/java/com/yoga/mborasystem/view/HomeFragment.java
+++ app/src/main/java/com/yoga/mborasystem/view/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.yoga.mborasystem.view;
 
 import static com.yoga.mborasystem.util.Ultilitario.alertDialog;
+import static com.yoga.mborasystem.util.Ultilitario.getAPN;
 import static com.yoga.mborasystem.util.Ultilitario.getIdIdioma;
 import static com.yoga.mborasystem.util.Ultilitario.getSelectedIdioma;
 import static com.yoga.mborasystem.util.Ultilitario.internetIsConnected;
@@ -299,7 +300,7 @@ public class HomeFragment extends Fragment {
                 return NavigationUI.onNavDestinationSelected(menuItem, navController);
             }
         }, getViewLifecycleOwner());
-
+        binding.textNomeUsuario.setText(getArguments().getString("nome", ""));
         requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), Ultilitario.sairApp(getActivity(), getContext()));
         return binding.getRoot();
     }
