0c1e257dcc932a6cd1db85fdb5ef0490d10337b2
António Teca <antonioteca@hotmail.com>
2022-09-28 01:12:50 +0100

    Guardar os dados boolean em modo privado.

--- app/src/main/java/com/yoga/mborasystem/util/Ultilitario.java
+++ app/src/main/java/com/yoga/mborasystem/util/Ultilitario.java
@@ -626,11 +626,11 @@ public class Ultilitario {
     }
 
     public static boolean getBooleanPreference(Context context, String idvalue) {
-        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(idvalue, false);
+        return context.getSharedPreferences("VALUE_BOOLEAN", Context.MODE_PRIVATE).getBoolean(idvalue, false);
     }
 
     public static void setBooleanPreference(Context context, boolean value, String idvalue) {
-        PreferenceManager.getDefaultSharedPreferences(context).edit().putBoolean(idvalue, value).apply();
+        context.getSharedPreferences("VALUE_BOOLEAN", Context.MODE_PRIVATE).edit().putBoolean(idvalue, value).apply();
     }
 
     public static String monthInglesFrances(String data) {
