6b07d4bbbcdd13a506ba1c4e29c2b3043aa074f2
António Teca <antonioteca@hotmail.com>
2022-12-08 10:05:36 +0100

    simplificar código.

--- app/src/main/java/com/yoga/mborasystem/view/CadastrarClienteFragment.java
+++ app/src/main/java/com/yoga/mborasystem/view/CadastrarClienteFragment.java
@@ -52,7 +52,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.gson.JsonObject;
 import com.koushikdutta.ion.Ion;
 import com.yoga.mborasystem.MainActivity;
