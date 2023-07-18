8edfd8c00608eda803e94ae901bbd1bdc3b2b096
António Teca <antonioteca@hotmail.com>
2023-07-18 18:38:20 +0100

    alterar nome de método

--- app/src/main/java/com/yoga/mborasystem/viewmodel/ClienteViewModel.java
+++ app/src/main/java/com/yoga/mborasystem/viewmodel/ClienteViewModel.java
@@ -260,7 +260,7 @@ public class ClienteViewModel extends AndroidViewModel {
     }
 
     @SuppressLint("CheckResult")
-    public void clienteExiste(boolean limitCadastro, Cliente c, Context context, View view, Activity activity) {
+    public void empresaExiste(boolean limitCadastro, Cliente c, Context context, View view, Activity activity) {
         executor = Executors.newSingleThreadExecutor();
         Handler handler = new Handler(Looper.getMainLooper());
         executor.execute(() -> {
@@ -447,7 +447,7 @@ public class ClienteViewModel extends AndroidViewModel {
                                 MainActivity.dismissProgressBar();
                                 showToast(getApplication().getApplicationContext(), Color.rgb(204, 0, 0), getApplication().getString(R.string.eqp_n_enc), R.drawable.ic_toast_erro);
                             } else
-                                clienteExiste(true, cliente, null, null, activity);
+                                empresaExiste(true, cliente, null, null, activity);
                         } catch (Exception ex) {
                             MainActivity.dismissProgressBar();
                             showToast(getApplication().getApplicationContext(), Color.rgb(204, 0, 0), "Cod. Team:\n" + ex.getMessage(), R.drawable.ic_toast_erro);
